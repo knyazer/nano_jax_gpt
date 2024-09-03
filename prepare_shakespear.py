40da430c20957196bc490e58b3dc231dfe087f0c
Roman Knyazhitskiy <git@knyaz.tech>
2024-09-03 20:53:44 +0200

    fix: pre-commit conflicts

--- prepare_shakespear.py
+++ prepare_shakespear.py
@@ -5,42 +5,38 @@ Will save train.bin, val.bin containing the ids, and meta.pkl containing the
 encoder and decoder and some other related info.
 """
 
-import os
 import pickle
+from pathlib import Path
 
-import numpy as np
 import requests
 
-# download the tiny shakespeare dataset
-input_file_path = os.path.join(os.path.dirname(__file__), "input.txt")
-if not os.path.exists(input_file_path):
+input_file_path = Path(__file__).parent / "input.txt"
+if not input_file_path.exists():
     data_url = (
         "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
     )
-    with open(input_file_path, "w") as f:
-        f.write(requests.get(data_url).text)
+    input_file_path.write_text(requests.get(data_url, timeout=60).text)
 
-with open(input_file_path) as f:
-    data = f.read()
+data = input_file_path.read_text()
 print(f"length of dataset in characters: {len(data):,}")
 
 # get all the unique characters that occur in this text
-chars = sorted(list(set(data)))
+chars = sorted(set(data))
 vocab_size = len(chars)
 print("all the unique characters:", "".join(chars))
 print(f"vocab size: {vocab_size:,}")
 
 # create a mapping from characters to integers
 stoi = {ch: i for i, ch in enumerate(chars)}
-itos = {i: ch for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}  # noqa
 
 
 def encode(s):
     return [stoi[c] for c in s]  # encoder: take a string, output a list of integers
 
 
-def decode(l):
-    return "".join([itos[i] for i in l])  # decoder: take a list of integers, output a string
+def decode(tokens):
+    return "".join([itos[i] for i in tokens])  # decoder: take a list of integers, output a string
 
 
 # create the train and test splits
@@ -54,11 +50,10 @@ val_ids = encode(val_data)
 print(f"train has {len(train_ids):,} tokens")
 print(f"val has {len(val_ids):,} tokens")
 
-# export to bin files
-train_ids = np.array(train_ids, dtype=np.uint16)
-val_ids = np.array(val_ids, dtype=np.uint16)
-train_ids.tofile(os.path.join(os.path.dirname(__file__), "train.bin"))
-val_ids.tofile(os.path.join(os.path.dirname(__file__), "val.bin"))
+current_dir = Path(__file__).parent
+
+train_ids.tofile(current_dir / "train.bin")
+val_ids.tofile(current_dir / "val.bin")
 
 # save the meta information as well, to help us encode/decode later
 meta = {
@@ -66,7 +61,7 @@ meta = {
     "itos": itos,
     "stoi": stoi,
 }
-with open(os.path.join(os.path.dirname(__file__), "meta.pkl"), "wb") as f:
+with (current_dir / "meta.pkl").open("wb") as f:
     pickle.dump(meta, f)
 
 # length of dataset in characters:  1115394
