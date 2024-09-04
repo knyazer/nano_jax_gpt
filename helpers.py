94878dd752a10b68065f7c35d3d1dc53bffdd5fd
Roman Knyazhitskiy <git@knyaz.tech>
2024-09-04 18:43:53 +0200

    feat: implement auto batch size adjustment for JAX training function

---
+++ helpers.py
@@ -0,0 +1,53 @@
+import time
+
+import jax
+
+
+def auto_batch_size_wrapper(fn, batch_size=0):
+    if batch_size != 0:
+        fn(batch_size)
+        return
+
+    batch_size = 4
+    print(f"""\033[34m
+Starting with a batch size of {batch_size}.
+Running a single step of training...
+\033[0m""")
+    for _ in range(10):
+        try:
+            # memory cleanup
+            for buf in jax.live_arrays():
+                buf.delete()
+
+            # run a single step of training
+            fn(batch_size, exit_after_first_step=True)
+            time.sleep(0.5)  # wait for python gc to cleanup
+
+            # load memory stats
+            memory_stats = jax.local_devices()[0].memory_stats()
+            assert memory_stats is not None, "Your devices don't support auto batch sizing :("
+            reserved = memory_stats["bytes_in_use"]
+            available_memory = float(memory_stats["bytes_limit"] - reserved)
+            peak_memory = float(memory_stats["peak_bytes_in_use"] - reserved)
+
+            if peak_memory >= available_memory * 0.90:
+                break
+            # we try to avoid rematerialization (perf reasons), so no overshooting allowed
+            new_batch_size = int(batch_size * (available_memory * 0.97 / peak_memory))
+            if new_batch_size == batch_size:
+                break
+            batch_size = new_batch_size
+            print(f"\033[34m\nNew batch size: {batch_size}\n\033[0m")
+
+        except Exception as e:
+            if "RESOURCE_EXHAUSTED" in str(e):
+                print(f"""
+        Failed due to an OOM: jax cannot resurrect after OOM usually, hence,
+        please, restart with a specified batch size lower than {batch_size}.""")
+                return
+
+    print(f"\033[32mFinal batch size: {batch_size}. Starting the training...\033[0m")
+
+    for buf in jax.live_arrays():
+        buf.delete()
+    fn(batch_size)
