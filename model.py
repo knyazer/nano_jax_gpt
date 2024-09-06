fix: adjust output probability cap and refine total loss calculation in model.py, update WandbLogger name in train.py

model.py
@@ -75,7 +75,7 @@ class MiddleHead(eqx.Module):
     def __call__(self, x: Float[Array, "embed"]):
         out = self.linear(self.lnorm(x))
         p = self.p_linear(jax.nn.gelu(out[: self.p_size], approximate=True))
-        return 0.9 * jax.nn.sigmoid(p), out[self.p_size :]
+        return jnp.min(jnp.array([jax.nn.sigmoid(p).ravel(), 0.7])), out[self.p_size :]
 
 
 class GPT(eqx.Module):
@@ -116,7 +116,6 @@ class GPT(eqx.Module):
         already_accepted_p = jnp.zeros((self.config.context_len,), dtype=jnp.float32)
         total_loss = jnp.zeros((self.config.context_len,), dtype=jnp.float32)
         current_estimated_compute = 4
-        reg_factor = 2
         accepts_log = []
 
         for block_index, (block, bkey) in enumerate(zip(self.blocks, keys)):  # todo: jax.lax.scan
@@ -130,10 +129,7 @@ class GPT(eqx.Module):
                 joint_block_p = (1.0 - already_accepted_p) * block_accept_p
                 accepts_log.append(joint_block_p.mean())
                 already_accepted_p += joint_block_p
-                total_loss += (
-                    joint_block_p * current_estimated_compute * loss
-                    + block_accept_p.mean() * reg_factor
-                )
+                total_loss += joint_block_p * current_estimated_compute * loss
 
         # total loss is expectation of the logit diff wrt
         x = eqx.filter_vmap(self.final_norm)(x)
@@ -144,7 +140,8 @@ class GPT(eqx.Module):
             total_loss += (1.0 - already_accepted_p) * loss * current_estimated_compute
         else:
             logits = self.lm_head(x[-1])
-        return logits, total_loss.sum() / current_estimated_compute, jnp.array(accepts_log)
+        final_loss = total_loss.sum() / current_estimated_compute
+        return logits, final_loss, jnp.array(accepts_log)
 
     def generate(self, idx, key, max_new_tokens=256):
         forward = eqx.nn.inference_mode(self)

train.py
@@ -21,7 +21,7 @@ from model import GPT
 
 initialise_tracking()
 
-wandb = WandbLogger(use_wandb=(jax.process_index() == 0), name="nano_jax_gpt_test")
+wandb = WandbLogger(use_wandb=(jax.process_index() == 0), name="max-dep-0.7")
 
 model_config: GPTConfig = GPTConfig.from_preset("chargpt")
 train_config: TrainConfig = TrainConfig.from_preset("chargpt")
@@ -147,9 +147,7 @@ def main(batch_size=train_config.batch_size, *, exit_after_first_step=False):
 
         for j, layer_p in enumerate(logs):
             log_dict[f"prob_{j}"] = layer_p
-            log_dict[f"logp_{j}"] = np.log(layer_p)
         log_dict[f"prob_{len(logs)}"] = 1 - logs.sum()
-        log_dict[f"logp_{len(logs)}"] = np.log(1 - logs.sum())
 
         wandb.log({"loss": loss.mean(), "step": i, **log_dict})
 