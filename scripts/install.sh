f1cbc3485a9a3ac7cd2a728b70c8cc0755a36ea7
Roman Knyazhitskiy <git@knyaz.tech>
2024-09-09 22:34:52 +0200

    refactor: remove btop and htop from installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-sudo apt install neofetch git btop htop build-essential -y
+sudo apt install neofetch git build-essential -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3.11 python3.11-venv -y
 
