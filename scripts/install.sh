c8c8db41e60716bf678028afbece38d866d7d569
Roman Knyazhitskiy <git@knyaz.tech>
2024-09-11 13:35:37 +0200

    refactor: update install script and remove run.bash, add nano_jax_gpt setup and pip install steps

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,17 @@
+sudo mount -t tmpfs -o size=150G tmpfs ~/.cache
+cd ~
+
 sudo apt install neofetch git build-essential -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3.11 python3.11-venv -y
 
-wget https://bootstrap.pypa.io/get-pip.py
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python3.11 get-pip.py
 
-python3.11 -m pip install -r tpu-requirements.txt -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+python3.11 -m pip install -r nano_jax_gpt/tpu-requirements.txt -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+
+sudo mount -t tmpfs -o size=100G tmpfs ~/nano_jax_gpt
+git clone https://github.com/knyazer/nano_jax_gpt
+
+cd ~/nano_jax_gpt/
+python3.11 prepare_openwebtext.py
--- scripts/run.bash
+++
@@ -1,3 +0,0 @@
-sudo mount -t tmpfs -o size=300G tmpfs ~
-python3.11 prepare_openwebtext.py
-python3.11 train.py
