badd11463aa660caa114a691768b2419acd38320
Roman Knyazhitskiy <git@knyaz.tech>
2024-09-11 16:52:47 +0200

    fix: update learning rate parameters and increase evaluation frequency for gpt2 configuration

--- configs.py
+++ configs.py
@@ -56,11 +56,11 @@ class TrainConfig(eqx.Module):
                     n_grad_accumulation=1,
                     train_for=600_000,
                     lr_config={
-                        "init_value": 6e-4,
-                        "peak_value": 6e-4,
+                        "init_value": 5e-4,
+                        "peak_value": 5e-4,
                         "warmup_steps": 1000,
                         "decay_steps": 600_000,
-                        "end_value": 6e-5,
+                        "end_value": 3e-5,
                     },
                     dataset_name="openwebtext",
                 )
@@ -83,6 +83,6 @@ class RunConfig(eqx.Module):
             case "chargpt":
                 return RunConfig()
             case "gpt2":
-                return RunConfig(times_to_eval=100, times_to_checkpoint=10, n_batches_in_eval=50)
+                return RunConfig(times_to_eval=200, times_to_checkpoint=10, n_batches_in_eval=50)
             case _:
                 raise AssertionError("Only 'chargpt' or 'gpt2' are allowed as presets!")
