a3827c1761cef0665ecfdc5b83551448ee943f38
Roman Knyazhitskiy <git@knyaz.tech>
2024-09-11 15:54:37 +0200

    refactor: remove auto batch size functionality and streamline training step implementation

--- configs.py
+++ configs.py
@@ -70,7 +70,6 @@ class TrainConfig(eqx.Module):
 
 class RunConfig(eqx.Module):
     n_devices: int = 1  # number of devices available
-    n_updates_on_device: int = 4  # how many steps to do without moving data to CPU
     times_to_checkpoint: int = 2  # how many times to checkpoint throughout the training
     times_to_eval: int = 10  # how many times to eval throughout training
     n_batches_in_eval: int = 20  # how many batches in eval
--- train.py
+++ train.py
@@ -17,7 +17,7 @@ from jaxtyping import Array, Int, PRNGKeyArray
 from tqdm import tqdm
 
 from configs import GPTConfig, RunConfig, TrainConfig
-from helpers import WandbLogger, auto_batch_size_wrapper
+from helpers import WandbLogger
 from model import GPT
 
 initialise_tracking()
@@ -32,7 +32,6 @@ jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")  # noqa
 jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
 jax.config.update("jax_persistent_cache_min_compile_time_secs", 0.1)
 
-wandb = WandbLogger(use_wandb=(jax.process_index() == 0), name="smol-chargpt")
 
 parser = argparse.ArgumentParser(description="Script to run with a model argument")
 parser.add_argument(
@@ -44,6 +43,7 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+wandb = WandbLogger(use_wandb=(jax.process_index() == 0), name=f"testing-{args.model}")
 
 model_config: GPTConfig = GPTConfig.from_preset(args.model)
 train_config: TrainConfig = TrainConfig.from_preset(args.model)
@@ -82,67 +82,62 @@ def get_batches(split: str, rng: np.random.Generator, shape: tuple):
     )
 
 
-@eqx.filter_value_and_grad
-def loss_fn(model: GPT, X: Int[Array, "batch ctx"], y: Int[Array, "batch ctx"], key: PRNGKeyArray):
+def loss_fn(
+    model: GPT, X: Int[Array, "batch ctx"], y: Int[Array, "batch ctx"], key: PRNGKeyArray | None
+):
     return eqx.filter_vmap(model)(X, y, key)[1].mean()
 
 
-def grad_acc_scan_fn(key: PRNGKeyArray, data: tuple, model: GPT):
-    X, y = data
-    loss, grads = loss_fn(model, X, y, jr.split(key, X.shape[:-1]))
-    return jr.split(key)[0], (loss, grads)
-
-
-def update_scan_fn(carry: tuple, data: tuple, model_s: GPT):
-    model_d, opt_state, key = carry
-    model = eqx.combine(model_d, model_s)
-    X, y = data
-
-    _, (loss, grads) = eqxi.scan(
-        eqx.Partial(grad_acc_scan_fn, model=model), key, (X, y), kind="lax"
-    )
-
-    loss = jax.tree.map(lambda x: jnp.mean(x), loss, is_leaf=eqx.is_inexact_array)
-    grads = jax.tree.map(lambda x: jnp.mean(x, axis=0), grads, is_leaf=eqx.is_inexact_array)
-
-    updates, opt_state = optim.update(grads, opt_state, model)
-    model = eqx.apply_updates(model, updates)
-    return (eqx.partition(model, eqx.is_array)[0], opt_state, jr.split(key)[0]), loss
-
-
-def upd_fn(model, opt_state, X, y, key, sharding):
+@eqx.filter_jit(donate="all")  # donate="all" allows to reuse args memory: free x2-ish memory
+def step_fn(model, optim, opt_state, X, y, key, sharding):
+    # shard the model and the data
     model, opt_state = eqx.filter_shard((model, opt_state), sharding.replicate())
     X, y = eqx.filter_shard((X, y), sharding)
 
+    # accumulation function: just compute the gradient and the loss
+    def grad_acc_scan_fn(key: PRNGKeyArray, data: tuple):
+        loss, grads = eqx.filter_value_and_grad(loss_fn)(
+            model, *data, key=jr.split(key, data[0].shape[:-1])
+        )
+        return jr.split(key)[0], (loss, grads)
+
+    # partition manually, since eqxi.scan does not do it automatically
     model_d, model_s = eqx.partition(model, eqx.is_array)
-    (model_d, opt_state, _), loss = jax.lax.scan(
-        eqx.Partial(update_scan_fn, model_s=model_s), (model_d, opt_state, key), (X, y)
-    )
-    return eqx.combine(model_d, model_s), opt_state, loss.mean()
+    _, (loss, grads) = eqxi.scan(grad_acc_scan_fn, key, (X, y), kind="lax")
 
+    # compute the mean loss and grads from the accumulated values
+    loss = jax.tree.map(lambda x: jnp.mean(x), loss, is_leaf=eqx.is_inexact_array)
+    grads = jax.tree.map(lambda x: jnp.mean(x, axis=0), grads, is_leaf=eqx.is_inexact_array)
 
-def eval_scan_fn(_, data, model):
-    losses = eqx.filter_vmap(model)(*data)[1].mean()
-    return None, losses
+    # step the model
+    updates, opt_state = optim.update(grads, opt_state, model)
+    model = eqx.apply_updates(model, updates)
 
-
-def evaluate(model, X, y, sharding):
-    model = eqx.filter_shard(model, sharding.replicate())
-    X, y = eqx.filter_shard((X, y), sharding)
-    _, losses = eqxi.scan(eqx.Partial(eval_scan_fn, model=model), None, (X, y), kind="lax")
-    return losses.mean()
+    return model, opt_state, loss.mean()
 
 
 evals_table = []
 
 
 def eval_fn(inference_model, eval_generator, batch_size, sharding):
+    def evaluate(model, X, y, sharding):
+        # shard the model
+        model = eqx.filter_shard(model, sharding.replicate())
+        X, y = eqx.filter_shard((X, y), sharding)
+
+        # accumulate loss for requested number of batches
+        _, losses = jax.lax.scan(
+            eqx.Partial(lambda _, data: loss_fn(model, *data, key=None)), None, (X, y)
+        )
+        return losses.mean()  # return the mean
+
     eval_x, eval_y = get_batches(
         "test", eval_generator, shape=(run_config.n_batches_in_eval, batch_size)
     )
     eval_x, eval_y = eqx.filter_shard((jnp.array(eval_x), jnp.array(eval_y)), sharding)
     eval_loss = float(eqx.filter_jit(evaluate)(inference_model, eval_x, eval_y, sharding).mean())
 
+    # generate a continuation for some random text, and record it to wandb
     test_sample = get_batches("test", np.random.default_rng(11), shape=(1,))[0][0]
     out = eqx.filter_jit(inference_model.generate)(idx=test_sample, key=jr.key(42))
     text = decode([int(x) for x in out])
@@ -152,81 +147,79 @@ def eval_fn(inference_model, eval_generator, batch_size, sharding):
 
 
 def main(batch_size=train_config.batch_size, *, exit_after_first_step=False):
-    model = GPT(jr.key(0), model_config)
-    n_model_params = jax.tree.map(lambda x: x.size, eqx.filter(model, eqx.is_array))
-    n_model_params = sum(jax.tree_leaves(n_model_params))
-    print(f"Model has {n_model_params/1_000_000:.2f}M parameters")
-    opt_state = optim.init(eqx.filter(model, eqx.is_array))
-    eval_loss = float(jnp.nan)
-
-    devices = mesh_utils.create_device_mesh(
-        (1, 1, jax.device_count(), 1)
-    )  # reps x accum x batch x data
-    sharding = jshard.PositionalSharding(devices)
-    replicated = sharding.replicate()
-    model = eqx.filter_shard(model, replicated)
-
-    def checkpoint(i):
-        Path("checkpoints").mkdir(exist_ok=True)
-        eqx.tree_serialise_leaves(f"checkpoints/model_{i}.eqx", model)
-        wandb.log_artifact("chk", f"checkpoints/model_{i}.eqx")
-
-    n_steps = train_config.train_for // run_config.n_updates_on_device
-
-    train_generator = np.random.default_rng(42)
-    eval_generator = np.random.default_rng(69)
-
-    def load_train_batches():
-        return get_batches(
-            "train",
-            train_generator,
-            shape=(
-                run_config.n_updates_on_device,
-                train_config.n_grad_accumulation,
-                batch_size,
-            ),
-        )
-
-    X, y = load_train_batches()
-    for i in (pbar := tqdm(range(n_steps))):
-        data_key, fwd_key = jr.split(jr.key(i))
-
-        t = time.time()
-
-        X, y = eqx.filter_shard((jnp.array(X), jnp.array(y)), sharding)
-        model, opt_state, loss = eqx.filter_jit(upd_fn, donate="all")(
-            model, opt_state, X, y, fwd_key, sharding
-        )
-        X, y = load_train_batches()  # async load
-        loss = float(loss.mean())  # wait for jax to finish
-        step_time = (time.time() - t) / run_config.n_updates_on_device
-
-        # log
-        pbar.set_description(f"loss:{loss:.2f} / eval:{eval_loss:.2f} | step:{step_time*1e3:.2f}ms")
-        if exit_after_first_step:
-            return
-        wandb.log({"loss": loss})
-
-        # if we want to checkpoint..
-        chckp_freq = n_steps // run_config.times_to_checkpoint
-        if i % chckp_freq == chckp_freq - 1:
-            checkpoint(i)
-
-        # if we want to eval...
-        eval_freq = n_steps // run_config.times_to_eval
-        if i % eval_freq == eval_freq - 1:
-            eval_loss = eval_fn(eqx.nn.inference_mode(model), eval_generator, batch_size, sharding)
-
-    checkpoint(train_config.train_for // run_config.n_updates_on_device)
-
-
-if __name__ == "__main__":
     optim = optax.chain(
         optax.clip_by_global_norm(train_config.global_norm),
         optax.adamw(
             optax.warmup_cosine_decay_schedule(**train_config.lr_config), weight_decay=1e-2
         ),
     )
-    auto_batch_size_wrapper(
-        main, batch_size=train_config.batch_size, n_devices=run_config.n_devices
-    )
+    model = GPT(jr.key(0), model_config)
+    n_model_params = jax.tree.map(lambda x: x.size, eqx.filter(model, eqx.is_array))
+    n_model_params = sum(jax.tree.leaves(n_model_params))
+
+    print(f"Model has {n_model_params/1_000_000:.2f}M parameters")
+    opt_state = optim.init(eqx.filter(model, eqx.is_array))
+    eval_loss = float(jnp.nan)
+
+    # we partition the data by the batch dimension
+    devices = mesh_utils.create_device_mesh((1, jax.device_count(), 1))  # grad_accum x batch x data
+
+    # sharding stuff
+    sharding = jshard.PositionalSharding(devices)
+    replicated = sharding.replicate()
+    model = eqx.filter_shard(model, replicated)
+
+    # random generator for numpy: cannot use jax due to synchronization
+    train_generator = np.random.default_rng(42)
+    eval_generator = np.random.default_rng(69)
+
+    def checkpoint(i):
+        Path("checkpoints").mkdir(exist_ok=True)  # create directory if not there
+        eqx.tree_serialise_leaves(f"checkpoints/model_{i}.eqx", model)
+        wandb.log_artifact("checkpoint", f"checkpoints/model_{i}.eqx")
+
+    def load_train_batches():  # simpler function to load train batches
+        return get_batches(
+            "train",
+            train_generator,
+            shape=(
+                train_config.n_grad_accumulation,
+                batch_size,
+            ),
+        )
+
+    X, y = load_train_batches()  # preload, so that later we can do async
+    for i in (pbar := tqdm(range(train_config.train_for))):
+        data_key, fwd_key = jr.split(jr.key(i))
+
+        t = time.time()
+
+        X, y = eqx.filter_shard((jnp.array(X), jnp.array(y)), sharding)  # shard preloaded data
+        model, opt_state, loss = step_fn(model, optim, opt_state, X, y, fwd_key, sharding)
+        X, y = load_train_batches()  # async load
+        loss = float(loss.mean())  # wait for jax to sync
+
+        # log
+        pbar.set_description(
+            f"loss:{loss:.2f} / eval:{eval_loss:.2f} | step:{(time.time() - t)*1e3:.2f}ms"
+        )
+        if exit_after_first_step:
+            return
+        wandb.log({"loss": loss})
+
+        # if we want to checkpoint..
+        chckp_freq = train_config.train_for // run_config.times_to_checkpoint
+        if i % chckp_freq == chckp_freq - 1:
+            checkpoint(i)
+
+        # if we want to eval...
+        eval_freq = train_config.train_for // run_config.times_to_eval
+        if i % eval_freq == eval_freq - 1:
+            eval_loss = eval_fn(eqx.nn.inference_mode(model), eval_generator, batch_size, sharding)
+
+    # checkpoint the final model
+    checkpoint(train_config.train_for)
+
+
+if __name__ == "__main__":
+    main()
