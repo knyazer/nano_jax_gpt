902edabaaae5e0bfc041b8ae7dae57405841a4a9
Roman Knyazhitskiy <git@knyaz.tech>
2024-09-30 00:10:11 +0200

    feat: log learning rate during training

--- train.py
+++ train.py
@@ -291,6 +291,7 @@ def main():  # noqa
                         "solver_error_correlation": err_rel,
                         "random_p_grad_1": state.prev_grads.blocks[3].proj_fc.weight.ravel()[157],
                         "random_p_grad_2": unscaled_grads.blocks[3].proj_fc.weight.ravel()[157],
+                        "lr": state.lr,
                     },
                     jnp.mod(t, 2) == 0,
                 )
